9a18f09492fc7fb03e9954d89e4a0c7e9f775f37
iTzFreeHD <jaaptijmes58@gmail.com>
2018-01-19 17:27:25 +0100

    Add files via upload

--- SignCommand Master/src/iTzFreeHD/Ssigncommand/Main.php
+++ SignCommand Master/src/iTzFreeHD/Ssigncommand/Main.php
@@ -2,7 +2,9 @@
 
 namespace iTzFreeHD\Ssigncommand;
 
-use pocketmine\block\block;
+use pocketmine\block\Block;
+use pocketmine\command\ConsoleCommandSender;
+use pocketmine\command\RemoteConsoleCommandSender;
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 use pocketmine\utils\TextFormat as c;
@@ -39,78 +41,91 @@ class Main extends PluginBase implements listener{
     public function onSignCange(SignChangeEvent $event){
             $player = $event->getPlayer();
             if (strtolower($event->getLine(0) === "[run]")) {
-			if ($player->hasPermission("command.sign") or $player->isOp()) {
-                            $event->setLine(0, c::GREEN."Command");
-                            if ($event->getLine(1) === "Player") {
-                               $event->setLine(1, c::YELLOW."Player"); 
+                if ($player->hasPermission("command.sign") or $player->isOp()) {
+                    $event->setLine(0, c::GREEN . "Command");
+                    if ($event->getLine(1) === "Player") {
+                        $event->setLine(1, c::YELLOW . "Player");
+                    } else {
+                        if ($event->getLine(1) === "VIP") {
+                            $event->setLine(1, c::GOLD . "VIP");
+                        } else {
+                            if ($event->getLine(1) === "Admin") {
+                                $event->setLine(1, c::RED . "Only Stuff");
                             } else {
-                                if ($event->getLine(1) === "VIP") {
-                                    $event->setLine(1, c::GOLD."VIP");
-                                } else {
-                                    if ($event->getLine(1) === "Admin") {
-                                        $event->setLine(1, c::RED."Only Stuff");
-                                    }
+                                if ($event->getLine(1) === "Console") {
+                                    $event->setLine(1, c::RED . "Console");
                                 }
                             }
-			} else {
-                            $event->getPlayer()->sendMessage($this->p.c::RED."Du hast keine Permissions");
-                            $event->setLine(0, "No Perm");
-                            $event->setLine(1, "ERROR");
-                            }
+                        }
+                    }
+                }else {
+                     $event->getPlayer()->sendMessage($this->p.c::RED."Du hast keine Permissions");
+                     $event->setLine(0, "No Perm");
+                     $event->setLine(1, "ERROR");
+                     }
+                 }
             }
-}
     public function onSignClick(PlayerInteractEvent $event){
         $player = $event->getPlayer();
 		$id = $event->getBlock()->getId();
 		if ($id === Block::SIGN_POST or $id === Block::WALL_SIGN) {
-			$tile = $event->getPlayer()->getLevel()->getTile($event->getBlock());
-			if ($tile instanceof \pocketmine\tile\Sign) {
-				$text = $tile->getText();
-                                $cmd = $text[2];
-				if (strtolower(c::clean($text[0])) === strtolower("Command")) {
-                    if ($text[1] === c::YELLOW."Player") {
+            $tile = $event->getPlayer()->getLevel()->getTile($event->getBlock());
+            if ($tile instanceof \pocketmine\tile\Sign) {
+                $text = $tile->getText();
+                $cmd = $text[2];
+                $ccmd = $cmd and $text[3];
+                if (strtolower(c::clean($text[0])) === strtolower("Command")) {
+                    if ($text[1] === c::YELLOW . "Player") {
                         if ($player->hasPermission("player.sign")) {
-
                             $this->getServer()->dispatchCommand($player, $cmd);
                         } else {
-                            $player->sendMessage($this->p.c::RED."Keine Rechte");
+                            $player->sendMessage($this->p . c::RED . "Keine Rechte");
                         }
                     }
-				    if ($text[1] === c::GOLD."VIP") {
+                    if ($text[1] === c::GOLD . "VIP") {
                         if ($player->hasPermission("vip.sign")) {
                             $this->getServer()->dispatchCommand($player, $cmd);
                         } else {
-                            $player->sendMessage($this->p.c::RED."Keine Rechte");
+                            $player->sendMessage($this->p . c::RED . "Keine Rechte");
                         }
                     }
-                    if ($text[1] === c::RED."Only Stuff") {
-                        if ($player->hasPermission("vip.sign")) {
+                    if ($text[1] === c::RED . "Only Stuff") {
+                        if ($player->hasPermission("admin.sign")) {
 
                             $this->getServer()->dispatchCommand($player, $cmd);
                         } else {
-                            $player->sendMessage($this->p.c::RED."Keine Rechte");
+                            $player->sendMessage($this->p . c::RED . "Keine Rechte");
                         }
                     }
-
-
-				}
-			}
-		}
+                    if ($text[1] === c::RED . "Console") {
+                        if ($player->hasPermission("console.sign")) {
+                            $this->getServer()->dispatchCommand(new ConsoleCommandSender(), $ccmd);
+                        } else {
+                            $player->sendMessage($this->p . c::RED . "Keine Rechte");
+                        }
+                    }
+                }
+            }
+        }
     }
-    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool {
-        if ($command->getName() === "sign") {
-            $sender->sendMessage(c::AQUA."Made by iTzFreeHD");
-            $sender->sendMessage(c::GRAY."--------------------");
-            $sender->sendMessage(c::GREEN."So benutz du SignCommand");
-            $sender->sendMessage(c::GREEN."Line1: [run]");
-            $sender->sendMessage(c::GREEN."Line2: Player, VIP, Admin");
-            $sender->sendMessage(c::GREEN."Line3: <Command>");
-            $sender->sendMessage(c::GREEN."Line4: <Beschreibung>");
-            $sender->sendMessage(c::GREEN."Alle Permissions:");
-            $sender->sendMessage(c::GREEN." Player -> player.sign");
-            $sender->sendMessage(c::GREEN." VIP -> vip.sign");
-            $sender->sendMessage(c::GREEN." Admin -> admin.sign");
-            $sender->sendMessage(c::GRAY."--------------------");
-            return false;}
-    }
+
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool
+        {
+            if ($command->getName() === "sign") {
+                $sender->sendMessage(c::AQUA . "Made by iTzFreeHD");
+                $sender->sendMessage(c::GRAY . "--------------------");
+                $sender->sendMessage(c::GREEN . "So benutz du SignCommand");
+                $sender->sendMessage(c::GREEN . "Line1: [run]");
+                $sender->sendMessage(c::GREEN . "Line2: Player, VIP, Admin");
+                $sender->sendMessage(c::GREEN . "Line3: <Command>");
+                $sender->sendMessage(c::GREEN . "Line4: <Beschreibung>");
+                $sender->sendMessage(c::GREEN . "Alle Permissions:");
+                $sender->sendMessage(c::GREEN . " Player -> player.sign");
+                $sender->sendMessage(c::GREEN . " VIP -> vip.sign");
+                $sender->sendMessage(c::GREEN . " Admin -> admin.sign");
+                $sender->sendMessage(c::GREEN . " Console -> console.sign");
+                $sender->sendMessage(c::GRAY . "--------------------");
+                return false;
+            }
+        }
 }
